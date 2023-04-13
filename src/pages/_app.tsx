d9b5f5c0806fb198cccfb251147f633d5d7aaa35
achkas0va <achkasovasasha80@gmail.com>
2023-04-13 10:45:03 +0300

    some canges

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,13 @@
+import Layout from '@/components/layout/Layout'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Layout>
+      <div onClick={(пше ) => {alert('привет')}}>ybybyb</div>
+      <div>xbxbxb</div>
+      <Component {...pageProps} />
+    </Layout>
+    )
 }
